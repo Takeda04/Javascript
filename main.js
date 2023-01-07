f90f662bb023263d237eab58fd7a9702aec64077
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-01-07 13:30:10 +0500

    finis

--- main.js
+++ main.js
@@ -11,6 +11,6 @@ alert(`Samalyot chiptasi ${mon * dollar} so'm`);
 alert(`Umumiy yotoqxona xarajatlari ${umumiy * dollar} so'm`);
 alert(`Muzey va ko’ngilochar joy xarajatlari ${relax * euro} so'm`);
 let kal = (`${(mon * dollar) + (umumiy * dollar) +(relax * euro)}`);
-alert(`Umumiy safarga umumiy borib kelish summasi ${(mon * dollar) + (umumiy * dollar) +(relax * euro)} so'm
+alert(`Umumiy safarga borib kelish summasi ${(mon * dollar) + (umumiy * dollar) +(relax * euro)} so'm
 ${ism}ning umumiy puli ${money} so'm 
 Safardan so'ng ${ism}da qoladi ${money - kal} so'm`)
