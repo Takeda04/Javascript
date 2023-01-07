30efd0d3dfd31a30521f24181a2f826476148b10
olimjon <olimjon@MacBook-Pro-olimjon.local>
2023-01-07 15:25:21 +0500

    fi

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 var dollar = 10650.34;
 var euro = 10650.5;
 let ism = prompt('Ismingizni kiriting')
-let money = prompt('Qancha pulingiz bor');
+let userMoney = prompt('Qancha pulingiz bor');
 
-let mon = prompt('Samalyot chiptasi necha dollar');
-let umumiy = prompt('Umumiy yotoqxona xarajatlar necha dollar');
+let ticketPrice = prompt('Samalyot chiptasi necha dollar');
+let hotelPrice = prompt('Umumiy yotoqxona xarajatlar necha dollar');
 let relax = prompt("Muzey va ko’ngilochar joy xarajatlari necha euro");
-alert(`${ism}da ${money} so'm pul bor`);
-alert(`Samalyot chiptasi ${mon * dollar} so'm`);
-alert(`Umumiy yotoqxona xarajatlari ${umumiy * dollar} so'm`);
+
+alert(`${ism}da ${userMoney} so'm pul bor`);
+alert(`Samalyot chiptasi ${ticketPrice * dollar} so'm`);
+alert(`Umumiy yotoqxona xarajatlari ${hotelPrice * dollar} so'm`);
 alert(`Muzey va ko’ngilochar joy xarajatlari ${relax * euro} so'm`);
-let kal = (`${(mon * dollar) + (umumiy * dollar) +(relax * euro)}`);
-alert(`Umumiy safarga borib kelish summasi ${(mon * dollar) + (umumiy * dollar) +(relax * euro)} so'm
-${ism}ning umumiy puli ${money} so'm 
-Safardan so'ng ${ism}da qoladi ${money - kal} so'm`)
+
+let moduleMoney = (`${(ticketPrice * dollar) + (hotelPrice * dollar) + (relax * euro)}`);
+
+alert(`Umumiy safarga borib kelish summasi ${(ticketPrice * dollar) + (hotelPrice * dollar) + (relax * euro)} so'm
+${ism}ning umumiy puli ${userMoney} so'm 
+Safardan so'ng ${ism}da qoladi ${userMoney - moduleMoney} so'm`);
